d86c01b7eb0726d4f08cf8649457f645d0a9f3e8
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-01 08:30:02 +0545

    hero layout complete

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
@@ -17,6 +18,15 @@ const Hero = () => {
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label="Shop now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )

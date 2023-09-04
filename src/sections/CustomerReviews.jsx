53bdfeb23507e8d5c0e7e70856833a3fd408d7bb
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-04 09:59:16 +0545

    review card start

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard'
 
 const CustomerReviews = () => {
   return (
@@ -10,7 +11,12 @@ const CustomerReviews = () => {
       <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review) => (
-          <ReviewCard />
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback} />
         ))}
       </div>
     </section>
